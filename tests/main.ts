4235300aeb8b1be4d2b1f28e88d15ec7bcfcb86c
Tiago Antao <tiagoantao@gmail.com>
2017-03-14 12:56:50 -0600

    test refactoring - ongoing

--- tests/main.ts
+++ tests/main.ts
@@ -3,12 +3,36 @@ import GenepopReader from '../src/main'
 import {assert} from 'chai'
 import {createReadStream} from 'fs'
 
-describe('Reader', () => {
-    it('Simple', () => {
-        let stream = createReadStream('tests/data/c2line.gen')
-        let reader = stream.pipe(new GenepopReader({}))
+const test_dir = 'tests/data/'
+const files = ['c2line.gen', 'c3line.gen', 'c2space.gen', 'c3space.gen',
+                'haplo3.gen', 'haplo2.gen']
+
+const test_files = (files: string[], done : Function) => {
+    let completed = 0
+    for (let file of files) {
+        let my_json = ''
+        console.log(file)
+        const single_file = test_dir + file
+        const stream = createReadStream(single_file)
+        const reader = stream.pipe(new GenepopReader({}))
         reader.on('data', (data : Buffer) => {
-            console.log(1, data.toString())
+            my_json += data.toString()
         })
+        reader.on('end', () => {
+            completed++
+            console.log(my_json, 99)
+            if (completed === files.length) done()
+        })
+    }
+}
+
+describe('Reader', () => {
+    it('Single file - basic', (done) => {
+        test_files([files[1]], done)
+    })
+
+    it('Several files - basic', (done) => {
+        ///test_files(files, done)
+        done()
     })
 })
